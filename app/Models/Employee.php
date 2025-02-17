4bd7577d40bf074927840187b7efa669e271b750
unknown <j.delima@mlgcl.edu.ph>
2025-02-17 17:22:46 +0800

    2-17-2025 activity 1

---
+++ app/Models/Employee.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Factories\HasFactory;
+
+class Employee extends Model
+{
+    //
+}
---
+++ database/migrations/2025_02_17_085437_create_table_student_list.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_student_list', function (Blueprint $table) {
+            $table->id();
+            $table->string('f_name');
+            $table->string('l_name');
+            $table->string('m_name');
+            $table->string('age');
+            $table->string('contact_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_student_list');
+    }
+};
