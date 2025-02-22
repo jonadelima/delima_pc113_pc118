14965c08533e8ba3471c4477e1f483167ada0ea8
unknown <j.delima@mlgcl.edu.ph>
2025-02-22 16:48:51 +0800

    2-19-2025 Activity 2

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -7,8 +7,17 @@ use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    public function index()
+
+    public function index(Request $request)
     {
-        return response()->json(Employee::all());
+        $query = Employee::query();
+        
+        if ($request->has('search')) {
+            $search = $request->search;
+            $query->where('name', 'LIKE', "%$search%")
+            ->orWhere('email', 'LIKE', "%$search%"); 
+        }
+
+        return response()->json($query->get(), 200);
     }
 }
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,8 +7,16 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json(Student::all());
+        $query = Student::query();
+        
+        if ($request->has('search')) {
+            $search = $request->search;
+            $query->where('name', 'LIKE', "%$search%")
+            ->orWhere('email', 'LIKE', "%$search%"); 
+        }
+
+        return response()->json($query->get(), 200);
     }
 }
